542a85731b33ae8b2a86b4db8525af77e0c46096
Thiago de Melo <tmelo.mat@gmail.com>
2025-01-27 20:52:49 -0300

    Add License

--- sudokuGUI.py
+++ sudokuGUI.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-# https://www.technologyreview.com/2012/01/06/188520/mathematicians-solve-minimum-sudoku-problem/
+
+# Copyright (c) 2025, Thiago de Melo <tmelo.mat@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# # https://www.technologyreview.com/2012/01/06/188520/mathematicians-solve-minimum-sudoku-problem/
 
 
 import tkinter as tk
